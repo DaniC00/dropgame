7727e85ed05e63533d22f0ccad505730d6bdc363
dcalper00 <dcalper00@gmail.com>
2021-01-14 18:51:14 +0100

    render bucket

--- desktop/src/com/mygdx/dropgame/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/dropgame/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.dropgame.DropGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "DropGame";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new DropGame(), config);
 	}
 }
